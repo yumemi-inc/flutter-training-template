cd27765bb6a3a9c57beabeb3bcbcc443fc41d930
blendthink <admin@blendthink.dev>
2022-10-31 21:02:38 +0900

    Create yumemi_weather package

---
+++ packages/yumemi_weather/example/yumemi_weather_example.dart
@@ -0,0 +1,6 @@
+import 'package:yumemi_weather/yumemi_weather.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/yumemi_weather/lib/yumemi_weather.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library yumemi_weather;
+
+export 'src/yumemi_weather_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
