8edcf51bcd233f03f5703b0bbecad73ba522eed0
morikann <mrknt0088@gmail.com>
2024-03-13 11:45:55 +0900

    chore: Add build.yaml to ignore lint for generated file

--- tools/cleanup_template/lib/src/clients/app_client.g.dart
+++ tools/cleanup_template/lib/src/clients/app_client.g.dart
@@ -1,5 +1,7 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+// ignore_for_file: type=lint, duplicate_ignore
+
 part of 'app_client.dart';
 
 // **************************************************************************
--- tools/cleanup_template/lib/src/services/flutter_sdk_service.g.dart
+++ tools/cleanup_template/lib/src/services/flutter_sdk_service.g.dart
@@ -1,5 +1,7 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+// ignore_for_file: type=lint, duplicate_ignore
+
 part of 'flutter_sdk_service.dart';
 
 // **************************************************************************
