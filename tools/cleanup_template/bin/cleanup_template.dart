3acddf639e48459bab1de23268e2b9a7eea08bc5
morikann <mrknt0088@gmail.com>
2024-03-13 10:45:08 +0900

    chore: Exec dart create -t console cleanup_template

---
+++ tools/cleanup_template/bin/cleanup_template.dart
@@ -0,0 +1,5 @@
+import 'package:cleanup_template/cleanup_template.dart' as cleanup_template;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cleanup_template.calculate()}!');
+}
---
+++ tools/cleanup_template/test/cleanup_template_test.dart
@@ -0,0 +1,8 @@
+import 'package:cleanup_template/cleanup_template.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
