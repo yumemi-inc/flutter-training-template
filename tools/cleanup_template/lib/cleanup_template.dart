82c65681f942971046b4991d31a771c1889dfc37
morikann <mrknt0088@gmail.com>
2024-03-13 11:34:44 +0900

    feat: Add process to install fvm

--- tools/cleanup_template/lib/cleanup_template.dart
+++ tools/cleanup_template/lib/cleanup_template.dart
@@ -1,5 +1,15 @@
+import 'dart:io';
+
 import 'package:cleanup_template/src/models/exit_status.dart';
 
 Future<ExitStatus> run() async {
+  // setup fvm
+  final result = Process.runSync('dart', ['pub', 'global', 'activate', 'fvm']);
+  final exitStatus = ExitStatus.fromCode(result.exitCode);
+  if (exitStatus != ExitStatus.success) {
+    print(result.stdout);
+    return exitStatus;
+  }
+
   return ExitStatus.success;
 }
